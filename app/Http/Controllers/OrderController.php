cc3b851e4dd3597bea34bd205942028d5acfaa96
Anmouer <55122038+Anmouer@users.noreply.github.com>
2020-10-23 17:52:01 +0900

    Update OrderController.php

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -12,37 +12,56 @@ use Illuminate\Support\Facades\Redirect;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
-{
+{   
+
+    public function saveTemOrder(Request $request) {
+        $order_user = $request->input('userName');
+        DB::table('oneprint_order')->insert([
+
+
+        ]);
+    }
+
+    public function getTemOrder(Request $request) {
+        $order_user = $request->input('userName');
+        $temOrder = DB::table('oneprint_order')->where('user_name',$user_name)->get();
+
+
+    }
+
+    public function deleteTemOrder(Request $request) {
+        $order_user = $request->input('userName');
+        DB::table('oneprint_order')->where('user_name',$user_name)->detele();
+
+    }
+
     public function addOrder(Request $request) {
 
-    $order_user = $request->input('userName');
-    $order_price = $request->input('price');
-    $order_type = $request->input('type');
-    $order_destcription = $request->input('destcription');
-    $order_email = $request->input('email');
-    $order_phonenum = $request->input('phonenum');
-    $order_subrub = $request->input('subrub');
-    $order_state = $request->input('state');
-    $order_postcode = $request->input('postcode');
-    $order_date = $request->input('orderDdata');
+        $order_user = $request->input('userName');
+        $order_price = $request->input('price');
+        $order_type = $request->input('type');
+        $order_destcription = $request->input('destcription');
+        $order_email = $request->input('email');
+        $order_phonenum = $request->input('phonenum');
+        $order_subrub = $request->input('subrub');
+        $order_state = $request->input('state');
+        $order_postcode = $request->input('postcode');
+        $order_date = $request->input('orderDdata');
 
-    $order_id = DB::table('oneprint_order')->order_by('order_id', 'desc')->first() + 1;
+        $order_id = DB::table('oneprint_order')->order_by('order_id', 'desc')->first() + 1;
 
-    DB::table('oneprint_order')->insert([
-        'order_name' => $order_name,
-        'order_type' => 0,
-        'order_password' => $order_password,
-        'order_email' => $order_email,
-        'order_phonenum' => $order_phonenum,
-        'order_address' => $order_address,
-        'order_subrub' => $order_subrub,
-        'order_state' => $order_state,
-        'order_postcode' => $order_postcode,
-        'order_date' => $order_date
-    ]);
-    $register = "success";
-    return response()->json($register);
-      
-
+        DB::table('oneprint_order')->insert([
+            'order_name' => $order_name,
+            'order_password' => $order_password,
+            'order_email' => $order_email,
+            'order_phonenum' => $order_phonenum,
+            'order_address' => $order_address,
+            'order_subrub' => $order_subrub,
+            'order_state' => $order_state,
+            'order_postcode' => $order_postcode,
+            'order_date' => $order_date
+        ]);
+        $register = "success";
+        return response()->json($register);
     }   
 }
